daa3f25b506a212f0bbdefcfa5b15446d6fd406a
BrianFontaine <brian02440@live.fr>
2020-05-15 23:47:41 +0200

    geek-wear_Creation_de_la_modal_Panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// creation de la nav-bar 
 let body = document.querySelector('body');
 let nav = document.createElement('nav');
 nav.className="navbar navbar-expand-lg navbar-light fixed-top";
+
 document.getElementById('header').insertAdjacentElement('afterbegin', nav);
 
 let logo = document.createElement('a');
@@ -20,32 +22,98 @@ for (let i = 0; i < list.length; i++) {
     menu.appendChild(menuList);
 }
 document.querySelector('a').insertAdjacentElement('afterend', menu);
-
-/*
-let gameBoy = document.createElement('div');
-gameBoy.id="gameboy";
-document.querySelector('main').insertAdjacentElement("afterbegin",gameBoy);
-
-document.getElementById('gameboy').className=" text-white justify-content-center d-flex bg-image";
-document.getElementById('gameboy').style.width="100%";
-document.getElementById('gameboy').style.height="378px";
-document.getElementById('gameboy').style.marginTop="150px"
+// fin nav-bar
 
 
-let vitre = document.createElement('div');
-vitre.id="vitre";
-document.getElementById('gameboy').insertAdjacentElement("afterbegin",vitre);
 
-document.getElementById('vitre').style.width="416px";
-document.getElementById('vitre').style.height="324px";
-document.getElementById('vitre').className=" mt-3 bg-img";
-*/
+// creation du boutton mdal pour le panier 
+let modalButton = document.createElement('button');
+modalButton.className="btn btn-primary mb-3";
+modalButton.dataset.toggle = 'modal';
+modalButton.dataset.target = '#exampleModalLong';
+menu.appendChild(modalButton);
+document.querySelector('ul').insertAdjacentElement('afterend', menu);
+// fin de la créationdu bouton 
 
+
+
+// création de la modal panier
+let divParentModal = document.createElement('div');
+divParentModal.id = "exampleModalLong";
+    divParentModal.className = "modal fade z-index";
+    divParentModal.setAttribute('tabindex','-1');
+    divParentModal.setAttribute('role','dialog');
+    divParentModal.setAttribute('aria-labelledby','exampleModalLongTitle');
+    divParentModal.setAttribute('aria-hidden','true');
+
+let childDivModal = document.createElement('div');
+    childDivModal.className="modal-dialog";
+    childDivModal.setAttribute('role','document');
+    divParentModal.appendChild(childDivModal);
+        
+let childDivModalContent = document.createElement('div');
+    childDivModalContent.className="modal-content";
+    childDivModal.appendChild(childDivModalContent)
+
+let childDivModalHeader = document.createElement('div');
+    childDivModalHeader.className="modal-header";
+    childDivModalContent.appendChild(childDivModalHeader);
+
+let childDivModalH5 = document.createElement('h5');
+    childDivModalH5.className="modal-title";
+    childDivModalH5.id="exampleModalLongTitle";
+    childDivModalH5.textContent="Votre Panier"
+    childDivModalHeader.appendChild(childDivModalH5);
+
+let childDivModalButton = document.createElement('button');
+    childDivModalButton.type="button";
+    childDivModalButton.className="close";
+    childDivModalButton.dataset.dismiss = 'modal';
+    childDivModalButton.setAttribute('aria-label','Close');
+    childDivModalHeader.appendChild(childDivModalButton);
+
+let childDivModalSpan = document.createElement('Span');
+    childDivModalSpan.setAttribute('aria-hidden','true');
+    childDivModalSpan.innerHTML="&times;"
+    childDivModalButton.appendChild(childDivModalSpan);
+
+let childDivModalContenerShop = document.createElement('div');
+    childDivModalContenerShop.className="modal-body";
+    childDivModalContenerShop.textContent="modal-bodymodal-bodymodal-bodymodal-bodymodal-bodymodal-bodymodal-body"
+    childDivModalContent.appendChild(childDivModalContenerShop);
+
+let childDivModalContenerFooter = document.createElement('div');
+    childDivModalContenerFooter.className="modal-footer";
+    childDivModalContent.appendChild(childDivModalContenerFooter);
+
+let childDivModalButtonClose = document.createElement('button');
+    childDivModalButtonClose.type="button";
+    childDivModalButtonClose.className="btn btn-secondary";
+    childDivModalButtonClose.textContent="Fermer";
+    childDivModalContenerFooter.appendChild(childDivModalButtonClose);  
+    
+let childDivModalButtonSave = document.createElement('button');
+    childDivModalButtonSave.type="button";
+    childDivModalButtonSave.className="btn btn-primary";
+    childDivModalButtonSave.textContent="Commander"
+    childDivModalContenerFooter.appendChild(childDivModalButtonSave);
+    
+    
+document.querySelector('ul').insertAdjacentElement('afterbegin', divParentModal);
+// fin de la creation modal panier
+
+
+
+
+// PacMan Menu
 var pacman = document.getElementById('#pacman');
 pacman.addEventListener('click', function(){
     pacman.className="rotate";
 
 });
+// Fin du PacMan Menu
+
+
 
 /* gameboy */
 
