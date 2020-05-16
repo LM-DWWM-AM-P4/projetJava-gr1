c71d6c98d17d1ae019519aa4b6a66ada76b6d5c5
mak <lievrard.maxime@icloud.com>
2020-05-16 11:01:23 +0200

    formulaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,3 +131,40 @@ gameboy.appendChild(btn2);
     }
     document.querySelector('main').insertAdjacentElement("afterbegin",divparent );
 // fin de la craetion de test 
+
+// tableau images
+
+imgArticles = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg'];
+
+// formulaire
+
+let classLabel = ['Nom', 'Prénom', 'Email']
+
+let form = document.createElement('form');
+document.querySelector('footer').insertAdjacentElement("afterbegin", form);
+
+for (let i = 0; i <= 3; i++) {
+
+    if (i <= 2) {
+    let formGroup = document.createElement('div');
+    formGroup.className = "form-group";
+    form.appendChild(formGroup);
+    
+    let label = document.createElement('label');
+    label.textContent = classLabel[i];
+    formGroup.appendChild(label);
+
+    let input = document.createElement('input');
+    input.className = "form-control"
+    formGroup.appendChild(input);
+    }
+
+    if (i == 3) {
+        let label = document.createElement('label');
+        label.textContent = "Objet";
+        form.appendChild(label);
+        let textarea = document.createElement('textarea');
+        textarea.className = "form-control";
+        form.appendChild(textarea);
+    }
+}
